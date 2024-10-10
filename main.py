1ae22910bd63c8853271719f42a1ffed208808bc
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-10 11:40:19 +0100

    build functionality and tests for a successful request to the GET /api/treasures endpoint

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 '''This module is the entrypoint for the `Cat's Rare Treasures` FastAPI app.'''
+from fastapi import FastAPI
+from db.connection import connect_to_db
+
+
+app = FastAPI()
+
+
+@app.get("/api/treasures")
+def get_all_treasures():
+    db = connect_to_db()
+    select_query = f"""
+        SELECT 
+            treasures.treasure_id, treasures.treasure_name, treasures.colour,
+            treasures.age, treasures.cost_at_auction, shops.shop_name
+        FROM treasures
+        JOIN shops ON treasures.shop_id = shops.shop_id
+        ORDER BY treasures.age;
+    """
+    treasures_data = db.run(sql=select_query)
+    column_names = [c["name"] for c in db.columns]
+    formatted_data = [dict(zip(column_names, treasure)) for treasure in treasures_data]
+    db.close()
+    return {"treasures": formatted_data}
--- tests/test_cats_rare_treasures.py
+++ tests/test_cats_rare_treasures.py
@@ -1,2 +1,35 @@
 '''This module contains the test suite for the
 `Cat's Rare Treasures` FastAPI app.'''
+from fastapi.testclient import TestClient
+from main import app
+from db.seed import seed_db
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def reset_db():
+    seed_db(env='test')
+
+
+class TestGetAllTreasures:
+    """
+    Test verifies:
+    - status code
+    - length of resulting list of treasure dicts
+    - treasure dicts content match the data types expected
+    - age of first treasure is lower than age of last treasure in the list
+    """
+    def test_200_returns_formatted_treasures(self):
+        client = TestClient(app)
+        response = client.get("/api/treasures")
+        body = response.json()
+        assert response.status_code == 200
+        assert len(body["treasures"]) == 26
+        for treasure in body["treasures"]:
+            assert type(treasure["treasure_id"]) == int
+            assert type(treasure["treasure_name"]) == str
+            assert type(treasure["colour"]) == str
+            assert type(treasure["age"]) == int
+            assert type(treasure["cost_at_auction"]) == float
+            assert type(treasure["shop_name"]) == str
+        assert body["treasures"][0]["age"] < body["treasures"][-1]["age"]
