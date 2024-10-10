9546060a3d346413b0a84eab4501491eee40bd72
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-10 16:47:25 +0100

    extend the functionality and tests of the GET /api/treasures endpoint to accept an 'order' query parameter, including error/exception handling logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''This module is the entrypoint for the `Cat's Rare Treasures` FastAPI app.'''
-from fastapi import FastAPI, Request, HTTPException, Query
+from fastapi import FastAPI, Request, HTTPException
 from db.connection import connect_to_db
 from pg8000.native import DatabaseError, identifier
 
@@ -8,11 +8,16 @@ app = FastAPI()
 
 
 @app.get("/api/treasures")
-def get_all_treasures(sort_by: str = "age"):
+def get_all_treasures(sort_by: str = "age", order: str = "asc"):
     if not sort_by:
         sort_by = "age"
     elif sort_by not in {"age", "cost_at_auction", "treasure_name"}:
         raise HTTPException(status_code=400, detail=f"Invalid sort_by value ({sort_by}) provided")
+    
+    if not order:
+        order = "ASC"
+    elif order not in {"asc", "desc"}:
+        raise HTTPException(status_code=400, detail=f"Invalid order value ({order}) provided")
     
     db = None
     try:
@@ -23,7 +28,7 @@ def get_all_treasures(sort_by: str = "age"):
                 treasures.age, treasures.cost_at_auction, shops.shop_name
             FROM treasures
             JOIN shops ON treasures.shop_id = shops.shop_id
-            ORDER BY treasures.{identifier(sort_by)};
+            ORDER BY treasures.{identifier(sort_by)} {identifier(order)};
         """
         treasures_data = db.run(sql=select_query)
         column_names = [c["name"] for c in db.columns]
--- tests/test_cats_rare_treasures.py
+++ tests/test_cats_rare_treasures.py
@@ -59,14 +59,39 @@ class TestGetAllTreasures:
             assert type(treasure["cost_at_auction"]) == float
             assert type(treasure["shop_name"]) == str
         assert costs_at_auction == sorted(costs_at_auction)
+
+    def test_200_returns_formatted_treasures_with_specified_sort_order(self, client):
+        """
+        Test verifies:
+        - status code
+        - length of resulting list of treasure dicts
+        - treasure dicts content match the data types expected
+        - treasures are sorted by the specified order
+        """
+        response = client.get("/api/treasures?order=desc")
+        treasures = response.json()["treasures"]
+        ages = [treasure["age"] for treasure in treasures]
+        assert response.status_code == 200
+        assert len(treasures) == 26
+        for treasure in treasures:
+            assert type(treasure["treasure_id"]) == int
+            assert type(treasure["treasure_name"]) == str
+            assert type(treasure["colour"]) == str
+            assert type(treasure["age"]) == int
+            assert type(treasure["cost_at_auction"]) == float
+            assert type(treasure["shop_name"]) == str
+        assert ages == sorted(ages, reverse=True)
         
     """
-    Error handling considerations for GET "/api/treasures":
-    - path is incorrect; 404 handled by FastAPI
-    - method does not exist; 405 handled by FastAPI
-    - parameter key, with empty value; implemented default to "age" column 
-    - parameter not allowed; custom 400 implemented
-    - db error; custom 500 implemented
+    Error handling considerations for GET "/api/treasures" are tested below, except for:
+    - sort_by parameter key, with empty value; implemented default to "age" column 
+    - order parameter key, with empty value; implemented default to "asc" 
+
+    These two cases are handled by the successful implementation tests above
+    """
+    
+    """
+    Path is incorrect; 404 handled by FastAPI
     """
     def test_404_if_path_is_incorrect(self, client):
         response = client.get("/api/treasure")
@@ -75,6 +100,9 @@ class TestGetAllTreasures:
             "detail": "Not Found"
         }
 
+    """
+    Method does not exist; 405 handled by FastAPI
+    """
     def test_405_if_method_does_not_exist(self, client):
         response = client.patch("/api/treasures")
         assert response.status_code == 405
@@ -82,13 +110,29 @@ class TestGetAllTreasures:
             "detail": "Method Not Allowed"
         }
 
+    """
+    Sort_by parameter not allowed; custom 400 implemented
+    """
     def test_400_if_specified_sort_column_not_allowed(self, client):
-        response = client.get("/api/treasures/?sort_by=thisdoesnotexist")
+        response = client.get("/api/treasures?sort_by=thisdoesnotexist")
         assert response.status_code == 400
         assert response.json() == {
             "detail": "Invalid sort_by value (thisdoesnotexist) provided"
         }
 
+    """
+    Order parameter not allowed; custom 400 implemented
+    """
+    def test_400_if_specified_order_not_allowed(self, client):
+        response = client.get("/api/treasures?order=notallowed")
+        assert response.status_code == 400
+        assert response.json() == {
+            "detail": "Invalid order value (notallowed) provided"
+        }
+
+    """
+    db error; custom 500 implemented
+    """
     @pytest.mark.xfail
     def test_500_custom_message_if_db_error(self, client):
         """
